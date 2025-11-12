d13d4aba005977d56567939d03413dd90ae2bf29
Rosetta Vang <you@example.com>
2025-11-12 16:30:45 -0500

    changes

--- calculator.py
+++ calculator.py
@@ -21,7 +21,7 @@ def hypotenuse(a, b):
 def add(a, b):
     return a + b
 
-def sub(a, b):
+def subtract(a, b):
     return a - b
 
 def mul(a, b):
@@ -32,7 +32,7 @@ def div(a, b):
         raise ValueError("Division by zero is not allowed")
     return a/b
 
-def log(a, b):
+def logarithm(a, b):
     if a <= 0 or a == 1:
         raise ValueError("Base must be positive and not equal to 1")
     if b <= 0:
