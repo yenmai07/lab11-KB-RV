6b3346666a7f5755b4d57119e3a34930bfe71514
Rosetta Vang <you@example.com>
2025-11-12 15:04:28 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -4,8 +4,25 @@ calculator.py
 
 One function per operation, in order.
 """
-# First example
-def add(a, b): 
-    pass
+import math
+def add(a, b):
+    return a + b
 
+def sub(a, b):
+    return a - b
 
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    try: b / a
+    except ZeroDivisionError:
+        print("error")
+
+def log(a, b):
+    try: math.log(a, b)
+    except ValueError:
+        print("error")
+
+def exp(a, b):
+    return a ** b
