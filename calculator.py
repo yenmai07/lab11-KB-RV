5bab18ac259cab682db8a7520e47748be17a6af9
Rosetta Vang <you@example.com>
2025-11-12 15:38:08 -0500

    modified calculator

--- calculator.py
+++ calculator.py
@@ -6,6 +6,13 @@ One function per operation, in order.
 """
 
 import math
+def square_root(a):
+    if a < 0:
+        raise ValueError("Cannot compute square root of negative number")
+    return math.sqrt(a)
+
+def hypotenuse(a, b):
+    return math.hypot(a, b)
 
 def add(a, b):
     return a + b
